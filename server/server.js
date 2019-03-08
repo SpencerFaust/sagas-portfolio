02ba5cbaee998e3bc47c2c78323e4d69ef6a9a94
Spencer Faust <faus0059@umn.edu>
2019-03-08 14:55:47 -0600

    Added server pool module, admin and client routes and updated routing on server.js.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const adminRoute = require('./routes/route.admin');
+const clientRoute = require('./routes/route.client');
 const port = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/admin', adminRoute);
+app.use('/client', clientRoute);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
