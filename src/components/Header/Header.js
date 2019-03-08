05a4a99dc2a6b1963fac7534a7f97ae6a511bea3
Spencer Faust <faus0059@umn.edu>
2019-03-08 15:09:57 -0600

    Added header to app, placeholder styling in header.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+
+class Header extends Component {
+    
+
+    render() {
+
+        return (
+         <div className="headerDiv">
+            Header
+         </div>
+        );
+    }
+}
+
+
+export default Header;
